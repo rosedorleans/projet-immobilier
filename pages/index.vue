1f5d43ba625ab1d8fa7835bfb5cf7c10f53300e6
Melodie <melodie.janvier@mail-ecv.fr>
2024-02-19 15:38:16 +0100

    modif homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>Bienvenue sur notre site de vente de maison</h1>
   <p>Vous voulez des maisons ? Nous en avons !</p>
-  <NuxtLink to="/posts/">Nos annonces</NuxtLink>
+  <div>
+    <NuxtLink to="/posts/">Nos annonces</NuxtLink>
+    <NuxtLink to="/contact/">Nous contacter</NuxtLink>
+  </div>
+  <p>Notre agence ? La meilleure ! Créée en 2024, nous avons l'expérience et la sagesse pour vous conseiller au mieux dans votre choix de maison. On ne vend pas d'appartement ni de studio, c'est pour les ploucs.</p>
 </template>
