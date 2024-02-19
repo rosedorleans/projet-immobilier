c4f5f1c60fdf4eb07b8f094fe0f54bcba9def160
Melodie <melodie.janvier@mail-ecv.fr>
2024-02-19 13:50:30 +0100

    homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Welcome to the homepage</h1>
     <AppAlert>
-      This is an auto-imported component.
+      This is an auto-imported component
     </AppAlert>
   </div>
 </template>
