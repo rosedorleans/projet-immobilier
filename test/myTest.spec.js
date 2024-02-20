40e2e85ac8cebf7cdf6a8999f102f5123f06f7e9
Rose Dorléans <rosedorleans1@gmail.com>
2024-02-20 09:32:28 +0100

    test h1 home maj

---
+++ test/myTest.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("Example test index", async ({ page }) => {
+  await page.goto("localhost:3000/");
+  const text = await page.textContent("h1");
+  expect(text).toBe("Bienvenue sur notre site de vente de maison");
+});
